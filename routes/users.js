58d9271dd36e58298c2b456c9aebeb820fd77c05
Brent Warren <macuser@Brentsmac.local>
2019-11-20 19:09:58 -0800

    base built out. database connected

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router();
+const ctrl = require('../controllers')
+
+// PATH = /api/v1/users
+
+// router.get('/:id', ctrl.users.show);
+
+module.exports = router;
