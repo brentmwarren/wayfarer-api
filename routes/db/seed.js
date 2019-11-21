167f7294684ada427db9b516856c5e5ef7768757
Brent Warren <macuser@Brentsmac.local>
2019-11-21 10:38:54 -0800

    seed data files started

---
+++ routes/db/seed.js
@@ -0,0 +1,15 @@
+const db = require('../models');
+
+const cityList = require('./cities.json');
+
+// removes all pokemon 
+db.Cities.remove({}, () => {
+	// loops through the json file
+	cityList.forEach(city => {
+		// for each one creates a pokemon entry in the DB
+		db.Cities.create(city, (error, createdCity) => {
+			if (error) return console.log(error);
+			console.log(createdCity);
+		});
+	});
+});
