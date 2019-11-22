2dee15ed1f6ce1361f3edfdd4dbd5a1e9a12cd1d
KennyK-TW <b2khakashu@yahoo.com>
2019-11-21 17:07:49 -0800

    pause for now

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
 module.exports = {
   auth: require('./auth'),
-  users: require('./users')
+  users: require('./users'),
 };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-const ctrl = require('../controllers')
+const ctrl = require('../controllers');
 
 // PATH = /api/v1/users
 
